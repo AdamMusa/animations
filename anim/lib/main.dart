e9e7a4863970bde6456cf3a14ddeb901e8b0dcbf
AdamMusa <adammusa2222@gmail.com>
2020-04-18 12:38:15 +0100

    animatedcrossfade

--- anim/lib/main.dart
+++ anim/lib/main.dart
@@ -11,13 +11,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  bool selected = false;
+
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
       appBar: AppBar(),
-      body:GestureDetector()
+      body:GestureDetector(
+        onTap: (){
+          setState(() {
+            selected = !selected;
+          });
+        },
+        child: AnimatedCrossFade(
+        firstChild: selected?Container(
+          width: selected?100:200,
+          height: selected?200:100,
+          child: Center(child: Text('animated')),
+          color: selected?Colors.red:Colors.green,
+        ):Center(child:Text('Oupps')),
+        secondChild: Container(
+          width: selected?100:200,
+          height: selected?200:100,
+          child: Center(child: Text('animated')),
+          color: selected?Colors.grey:Colors.blueAccent
+        ), 
+        crossFadeState:CrossFadeState.showFirst,
+        duration: Duration(seconds: 3),
+        firstCurve: Curves.fastOutSlowIn,
+        secondCurve: Curves.fastLinearToSlowEaseIn,
+        sizeCurve: Curves.linear
+        ),
+      )
     );
   }
 }
