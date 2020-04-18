d5c12d9d95956944ba88225c9838a559871345e3
AdamMusa <adammusa2222@gmail.com>
2020-04-18 15:44:17 +0100

    initial

---
+++ animationDefaultText/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: MyHomePage()
+));
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(),
+      body:GestureDetector(
+       
+      )
+    );
+  }
+}
