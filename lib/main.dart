245cb5907165316b7f9ea62da79a6a78b0c81b01
AdamMusa <adammusa2222@gmail.com>
2020-04-18 11:48:57 +0100

    added title in appBar

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('AnimationContainer'),
+      ),
       body: GestureDetector(
         onTap:(){
           setState(() {
